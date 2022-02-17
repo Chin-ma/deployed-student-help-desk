1da2a05d90ad77f158ca6156cd6f1ca37a077954
chinmay <chinmay2003cp@gmail.com>
2022-02-17 12:03:24 +0530

    Fixed the issue of remotemysql

--- Query.php
+++ Query.php
@@ -1,20 +1,18 @@
 <?php
 
-include 'Config.php';
+include_once 'Config.php';
 
 $name = $_POST['name'];
 $subject = $_POST['subject'];
 $querytext = trim($_POST['querytext']);
 
-$sql = "INSERT INTO queries (name, subject, querytext, querytime) VALUES ('$name', '$subject', '$querytext', CURRENT_TIMESTAMP())";
+$sql = "INSERT INTO queries (`name`, `subject`, `querytext`, `querytime`) VALUES ('$name', '$subject', '$querytext', CURRENT_TIMESTAMP())";
 
 if ($conn->query($sql) === TRUE) {
-    echo 'Details added';
+    header('Location: ViewQuery.php');
 } 
 else {
     echo 'Error'. $sql . 'br' . $conn->error;
 }
-
-header('Location: ViewQuery.php');
 
 ?>
